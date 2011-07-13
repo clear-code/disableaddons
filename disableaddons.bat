b93b292251b860a6548c5e1a7c5166f91c5bc754
SHIMODA Hiroshi <shimoda@clear-code.com>
2011-07-13 09:40:34 +0000

    Fix some scripts for build

--- disableaddons.bat
+++ disableaddons.bat
@@ -1,5 +1,3 @@
 set appname=%~n0
 
-copy buildscript\make_new.sh .\
-bash make_new.sh %appname% version=0
-del make_new.sh
+bash makexpi.sh %appname% version=0
--- forceupdate/disableaddons-forceupdate.sh
+++ forceupdate/disableaddons-forceupdate.sh
@@ -3,9 +3,7 @@
 appname=${0##*/}
 appname=${appname%.sh}
 
-chmod -cfr 644 *.jar *.js *.light *.inf *.rdf *.cfg *.manifest
+chmod -cfR 644 *.jar *.js *.light *.inf *.rdf *.cfg *.manifest
 rm $appname.jar
 
 zip -r0 "$appname.jar" content locale skin -x \*/.svn/\*
-
-
