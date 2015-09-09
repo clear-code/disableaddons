4046bd34673a0b99871ede9f304c1a547f3ac493
YUKI Hiroshi <shimoda@clear-code.com>
2015-09-09 17:02:48 +0900

    Disable background connection about addon manager

--- defaults/preferences/disableaddons.js
+++ defaults/preferences/disableaddons.js
@@ -5,4 +5,19 @@ pref("extensions.update.enabled", false);
 pref("xpinstall.enabled", false);
 pref("extensions.showMismatchUI", false);
 
+//We should keep the blocklist available, because it provides more information not only blocked addons...
+// lockPref("extensions.blocklist.enabled", false);
+// lockPref("extensions.blocklist.detailsURL", "");
+// lockPref("extensions.blocklist.itemURL", "");
+// lockPref("extensions.blocklist.url", "");
+
+lockPref("extensions.getAddons.get.url", "");
+lockPref("extensions.getAddons.getWithPerformance.url", "");
+lockPref("extensions.getAddons.recommended.url", "");
+lockPref("extensions.getAddons.search.browseURL", "");
+lockPref("extensions.getAddons.search.url", "");
+
+//Because the "share" button is out of control, keep it available.
+// lockPref("extensions.webservice.discoverURL", "");
+
 pref("extensions.disableaddons@clear-code.com.disable.manager", true);
